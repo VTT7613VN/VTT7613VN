1b7798c8c0835228402a1848e65789e6af72e449
VTTDEV2613 <165543259+VTT7613VN@users.noreply.github.com>
2026-03-01 18:42:10 +0700

    Update YT.js

--- YT.js
+++ YT.js
@@ -1,9 +1,9 @@
-// Đoạn mã thay thế văn bản
-let body = $response.body;
-if (body) {
-    // Tìm và thay thế chữ YouTube thành Premium
-    body = body.replace(/"text":"YouTube"/g, '"text":"Premium"');
-    $done({ body });
-} else {
-    $done({});
+/*
+* Script đổi logo YouTube Premium 2026
+*/
+let obj = JSON.parse($response.body);
+if (obj.responseContext) {
+    // Mã này can thiệp vào luồng dữ liệu hiển thị của YouTube
+    obj.logo = {"premiumLogoRenderer": {"unlimitedSubscribedBadge": {"unlimitedBadgeRenderer": {"badge": {"item": {"unlimitedBadgeContentsRenderer": {"icon": {"iconType": "YOUTUBE_LOGO_PREMIUM"}}}}}}}};
 }
+$done({body: JSON.stringify(obj)});
